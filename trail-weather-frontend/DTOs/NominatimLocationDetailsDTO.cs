71670400d601e7893f596043e416d3cacb4f6b27
MarekHayden <88005118+HaydenMarek@users.noreply.github.com>
2025-02-05 22:34:40 +0100

    ux

---
+++ trail-weather-frontend/DTOs/NominatimLocationDetailsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace trail_weather_frontend.DTOs
+{
+    public class NominatimLocationDetailsDTO
+    {
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string display_name { get; set; }
+    }
+}
